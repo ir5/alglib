3056c06564fff9bc75fd3330c2556289b4c01bee
Mitsuru Kusumoto <kusumoto.mitsuru@gmail.com>
2015-03-01 16:18:00 +0900

    Change using alias of ll to typedef because it failed to compile in AOJ...

--- library/template/template.cpp
+++ library/template/template.cpp
@@ -8,7 +8,7 @@
 #include <sstream>
 #include <set>
 using namespace std;
-using ll = long long;
+typedef long long ll;
 
 class range {private: struct I{int x;int operator*(){return x;}bool operator!=(I& lhs){return x<lhs.x;}void operator++(){++x;}};I i,n;
 public:range(int n):i({0}),n({n}){}range(int i,int n):i({i}),n({n}){}I& begin(){return i;}I& end(){return n;}};
